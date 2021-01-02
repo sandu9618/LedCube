0a8b6f9578aeffbcdd2786580ff1e7674012b6a7
sandu9618 <sandunipavithra9611@gmail.com>
2021-01-02 11:15:51 +0530

    First commit

---
+++ test.ino
@@ -0,0 +1,17 @@
+ 
+ //1
+
+void setup()
+{
+  pinMode(1,OUTPUT)
+}
+//xxxxxxxxxxxxxxxxxxxxFUNCTION LOOPxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+void loop()
+{
+  digitalWrite(1,HIGH)
+  delay(1000)
+  digitalWrite(1,LOW)
+  delay(1000)
+}
